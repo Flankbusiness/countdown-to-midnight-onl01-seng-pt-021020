b890b5baf24fc9a8ce73dd47f700ae6914f6c5a4
Steven Prindle <flankbusiness@gmail.com>
2020-02-15 22:03:28 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,29 @@
 #write your code here
-
 def countdown
-end
+ number = 10
+  while number > 0 
+   puts "#{number} SECOND(S)!"
+    number -= 1
+    while number == 0 
+    puts "HAPPY NEW YEAR!"
+    break
+   end
+  end
+ end
+countdown
+
+def countdown_with_sleep
+ number = 5
+  while number > 0 
+  sleep (1) 
+   puts "#{number} SECOND(S)!"
+    number -= 1
+    while number == 0 
+    sleep(1)
+    puts "HAPPY NEW YEAR!"
+    break
+   end
+  end
+ end
+countdown_with_sleep
+

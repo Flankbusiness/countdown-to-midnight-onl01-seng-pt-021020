b2507dcbf5e946126c7361eced15322318e95e3b
Steven Prindle <flankbusiness@gmail.com>
2020-02-15 22:12:51 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -8,16 +8,12 @@ def countdown(seconds)
  end
 countdown(10)
 
-def countdown_with_sleep
- seconds = 2
+def countdown(seconds)
   while seconds > 0 
-  sleep (6) 
-   puts "#{seconds} more second(s) to sleep!"
-    seconds-= 1
-    while seconds == 0 
-    puts "HAPPY NEW YEAR!"
-    break
-   end
+   puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+    sleep (1)
   end
+  return "HAPPY NEW YEAR!"
  end
-countdown_with_sleep
+countdown(10)
